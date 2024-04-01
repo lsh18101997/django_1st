18be02697429ed1841ca2ab8f86b6dbf558fe41f
Leesohyun <vir5312@naver.com>
2024-04-01 16:25:01 +0900

    modified some files

--- polls/urls.py
+++ polls/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("index1/",views.index1, name="index1"),
-    path("index2/", views.index2, name="index2"),
-    path("index3/", views.index3, name="index3"),
     # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
--- polls/views.py
+++ polls/views.py
@@ -1,37 +1,17 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.db.models.functions import Greatest
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-
+from django.urls import reverse
 from .models import Question, Choice
-from datetime import datetime
 
-current_day = datetime.today().day
-current_year = datetime.today().year
 
 def index(request):
     total_question_number = Question.objects.count()
-    latest_question_list = Question.objects.order_by("-pub_date")[:10]
-    before_today_question_list = Question.objects.filter(pub_date__day__lt=current_day)[:10]
     question_list = Question.objects.all()
-    vote_morethanzero_question_list = [i.choice_set.filter(votes__gt=0) for i in question_list]
     context = {"total_question_number": total_question_number,
-               "latest_question_list": latest_question_list, 
-               "before_today_question_list": before_today_question_list,
-               "vote_morethanzero_question_list": vote_morethanzero_question_list}
+               "question_list": question_list}
     return render(request, "polls/index.html", context)
-
-def index1(request):
-    question_list = Question.objects.filter(pub_date__day=current_day)
-    context = {"question_list" : question_list}
-    return render(request, "polls/index1.html", context)
-
-def index2(request):
-    response = "Hello, world!"
-    return HttpResponse(response)
-
-def index3(request):
-    year_question_list = Question.objects.filter(pub_date__year=current_year)
-    context = {"year_question_list" : year_question_list}
-    return render(request, "polls/index3.html", context)
 
 def detail(request, question_id):   # 매개변수 이름과 polls.urls.py의 path에 있는 <int:>안의 이름이 같아야 함.
     question = get_object_or_404(Question, pk=question_id)
@@ -39,11 +19,31 @@ def detail(request, question_id):   # 매개변수 이름과 polls.urls.py의 pa
     return render(request, "polls/detail.html", {"question": question, "choice_list": choice_list})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        # selected_choice.votes = Greatest(F("votes") - 1, 0)
+        # selected_choice.votes = F("votes") * 2
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 def question_id(request, question_id):
     return HttpResponse("This Question's question_id is %s" % question_id)
